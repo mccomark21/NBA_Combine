dbc0bd9fae6b294d57b8d6a6b6d1c894efa53e6b
mccomark21 <mccomark21@gmail.com>
2022-11-09 05:48:47 +0000

    test 2

--- git_test.R
+++ git_test.R
@@ -1,3 +1,9 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+# personal access token: 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
+
+git config --global user.name "mccomark21"
+git config --global user.email "mccomark21@gmail.com"
+git config -l
